11f08da7349faec34ea74543053d6ae415ac7b0f
jasonc-dev <jasoncohen07@gmail.com>
2021-01-27 11:25:04 +0000

    fixing null pointer exception bug

--- src/main/java/guru/sfg/beer/inventory/service/services/DeallocationListener.java
+++ src/main/java/guru/sfg/beer/inventory/service/services/DeallocationListener.java
@@ -2,6 +2,7 @@ package guru.sfg.beer.inventory.service.services;
 
 import guru.sfg.beer.inventory.service.config.JmsConfig;
 import guru.sfg.brewery.model.events.AllocateOrderRequest;
+import guru.sfg.brewery.model.events.DeallocateOrderRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jms.annotation.JmsListener;
@@ -13,8 +14,8 @@ import org.springframework.stereotype.Component;
 public class DeallocationListener {
     private final AllocationService allocationService;
 
-    @JmsListener(destination = JmsConfig.ALLOCATE_ORDER_QUEUE)
-    public void listen(AllocateOrderRequest request) {
+    @JmsListener(destination = JmsConfig.DEALLOCATE_ORDER_QUEUE)
+    public void listen(DeallocateOrderRequest request) {
         allocationService.deallocateOrder(request.getBeerOrderDto());
     }
 }
